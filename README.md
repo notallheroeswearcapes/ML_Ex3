8caaed78e963ed41fc575d68310c01c5f3843679
Matthias Eder <eder.matthias1@gmail.com>
2021-02-02 17:31:29 +0100

    adapted package structure

--- README.md
+++ README.md
@@ -10,12 +10,16 @@
 We stick to the official Python packaging guide (https://packaging.python.org/tutorials/packaging-projects/) to create 
 the setup.py file  
 We use the package 'click' to create a command-line interface (https://click.palletsprojects.com/en/7.x/).
+The datasets CIFAR-10 and The German Traffic Sign Recognition Benchmark build the basis of our analysis.
+We use MLP, x and y as algorithms.
 
 ## Packages
 
 | Package      	| Version 	|
 |--------------	|---------	|
-| Python       	| 3.9   	|
+| Python       	| 3.8.5   	|
 | click        	| 7.1.2 	|
+| setuptools   	| 53.0.0 	|
+| wheel        	| 0.36.2 	|
 
 ## Guide
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="exercise3-group09-ml-tu-ws20",
+    version="0.0.1",
+    author="Matthias Eder",
+    author_email="e1624856@student.tuwien.ac.at",
+    description="The third exercise of group 09 for the Machine Learning course of TU Wien in the winter semester of "
+                "2020/2021.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/notallheroeswearcapes/ML_Ex3",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8.5',
+)
